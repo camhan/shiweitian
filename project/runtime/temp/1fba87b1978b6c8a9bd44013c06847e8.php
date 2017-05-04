84ee74a9baff30c7a80535e94e9418df71c170e2
hanjian <602966250@qq.com>
2017-05-04 16:32:02 +0800

    16:31

---
+++ project/runtime/temp/1fba87b1978b6c8a9bd44013c06847e8.php
@@ -0,0 +1,45 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:99:"B:\Cam1\wamp\phpStudy\WWW\php10\shiweitian\project\public/../application/home\view\show\index1.html";i:1493295416;}*/ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<base href="__PUBLIC__/home/more/">
+<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="author" content="FamousThemes" />
+<meta name="description" content="Get in the spotlight" />
+<meta name="keywords" content="premium css templates, premium wordpress themes, famous themes, themeforest" />
+<title>Show</title>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+<!-- Hide Mobiles Browser Navigation Bar -->
+<script type="text/javascript">
+window.addEventListener("load",function() {
+	// Set a timeout...
+	setTimeout(function(){
+	// Hide the address bar!
+	window.scrollTo(0, 1);
+	}, 0);
+});
+</script>
+</head>
+<body>
+<div id="main_container">
+	<div class="logo">
+    </div>
+<div class="menu">
+<ul>
+
+<li><a href="<?php echo url('index/index'); ?>"><img src="images/icon4.png" border="0" alt="" title=""/>点餐</a></li>
+<li><a href="<?php echo url('user/save'); ?>"><img src="images/icon1.png" border="0" alt="" title=""/>完善信息</a></li>	
+<li><a href="<?php echo url('show/u_order'); ?>"><img src="images/icon3.png" border="0" alt="" title=""/>个人订单</a></li>
+<li><a href="<?php echo url('service/index1'); ?>"><img src="images/icon2.png" border="0" alt="" title=""/>客服</a></li>
+<li><a href="<?php echo url('login/loginout'); ?>"><img src="images/icon6.png" border="0" alt="" title=""/>退出登录</a></li>
+<li><a href="<?php echo url('index/index'); ?>"><img src="images/icon6.png" border="0" alt="" title=""/>待开发ing</a></li>
+
+
+
+</ul>
+</div>
+
+</div>
+</body>
+</html>
